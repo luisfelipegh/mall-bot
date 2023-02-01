35f3d27458218f9c19871e185c04d37c1554ceed
Luis Garcia <luis.garciaherrera@evertecinc.com>
2023-02-01 17:45:15 -0500

    add initial version

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 require 'vendor/autoload.php';
 require_once 'File.php';
+const CHANNEL = 'bots';
+const WEB_SOCKET_URL = "ws://ws-us2.pusher.com:80/app/128b02cc77db3e123848?version=7.0.3&protocol=5";
 
-$websocketUrl = '';
-
-$client = new WebSocket\Client($websocketUrl, [
+$client = new WebSocket\Client(WEB_SOCKET_URL, [
         'timeout' => 60
     ]
 );
@@ -13,19 +13,23 @@ $client = new WebSocket\Client($websocketUrl, [
 $client->text(json_encode([
     'event' => 'pusher:subscribe',
     'data' => [
-        'channel' => 'bot'
+        'channel' => CHANNEL
     ]
 ]));
 
 while (true) {
     try {
-        $message = json_decode($client->receive());
+        $payload = json_decode($client->receive());
+        $event = $payload->event ?? null;
+        $data = json_decode($payload?->data);
 
-        echo $message;
+        dump('nuevo evento "' . $event . '" recibido: ' . json_encode($data));
 
     } catch (WebSocket\TimeoutException $exception) {
 
     }
 }
+
+File::saveClientDelivery([]);
 
 $client->close();
